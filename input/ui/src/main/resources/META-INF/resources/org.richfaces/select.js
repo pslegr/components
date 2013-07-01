31395649481e60b0dfb33cb4a066de86b1ea4626
jigg4 <jwitowski@gmx.de>
2013-07-01 08:33:17 +0200

    RF-11021: first Item is shown if defaultLabel is not set

--- input/ui/src/main/resources/META-INF/resources/org.richfaces/select.js
+++ input/ui/src/main/resources/META-INF/resources/org.richfaces/select.js
@@ -1,3 +1,25 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 (function ($, rf) {
 
     rf.ui = rf.ui || {};
@@ -46,6 +68,20 @@
             this.cache = new rf.utils.Cache("", this.originalItems, getData, true);
         }
         this.changeDelay = mergedOptions.changeDelay;
+
+        if (this.defaultLabel == "") {
+            var firstItem = this.clientSelectItems[0];
+            var key = $(firstItem).attr("id");
+            var label;
+            $.each(this.clientSelectItems, function() {
+                if (this.id == key) {
+                    label = this.label;
+                    return false;
+                }
+            });
+            this.__setValue(label);
+            this.__save();
+        }
     };
 
     rf.ui.InputBase.extend(rf.ui.Select);
